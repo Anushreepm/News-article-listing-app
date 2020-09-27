2ae91bd8516f8595cdf9538cdc1f6a7cbab81181
Anushree P M <anushreepm13@gmail.com>
2020-09-27 19:25:18 +0530

    Updates

---
+++ precache-manifest.46c6bf7e1afd0b41627678d1ee497923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d913cccae779c1c1a269fd0d7af9594",
+    "url": "/News-article-listing-app/index.html"
+  },
+  {
+    "revision": "4498871b1fc6628d85c2",
+    "url": "/News-article-listing-app/static/css/main.08390d45.chunk.css"
+  },
+  {
+    "revision": "457615401899f974e473",
+    "url": "/News-article-listing-app/static/js/2.ffe9af7e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/News-article-listing-app/static/js/2.ffe9af7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4498871b1fc6628d85c2",
+    "url": "/News-article-listing-app/static/js/main.cda957cf.chunk.js"
+  },
+  {
+    "revision": "c3581a3588beb3681986",
+    "url": "/News-article-listing-app/static/js/runtime-main.ff3dcb09.js"
+  }
+]);
